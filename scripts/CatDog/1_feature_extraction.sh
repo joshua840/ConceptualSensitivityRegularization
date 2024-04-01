7289c21cc70827d6b329f44896fd38f181818e9a
joshua840 <shjoo840@gmail.com>
2024-04-01 21:00:00 +0900

    num_worker changed

--- scripts/CatDog/1_feature_extraction.sh
+++ scripts/CatDog/1_feature_extraction.sh
@@ -11,7 +11,7 @@ CUDA_VISIBLE_DEVICES=0 python3 -m csr.main_run fit \
  --trainer configs/FeatureExtractor/trainer.yaml \
  --model configs/FeatureExtractor/LitFeatureExtraction.yaml \
  --model.dataset catdog_concepts \
- --model.data_dir /media/disk2/Data \
+ --model.data_dir /media/disk2/Data/Places365 \
  --model.save_root /media/disk2/Data/Features \
  --model.nimg_per_concept 128 \
  --seed_everything 1234 \

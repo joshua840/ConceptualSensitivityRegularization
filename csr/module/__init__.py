629bd74aded2d9fbf475686664b2ed62b06dcd6f
Sunghwan Joo <shjoo840@gmail.com>
2024-04-01 12:55:12 +0900

    Revert "Joshua840/issue1"

--- csr/module/__init__.py
+++
@@ -1,8 +0,0 @@
-from .erm import ERM
-from .gdro import GDRO
-from .cgr import CGR
-from .jtt import JTT, JTTMetadataGenerator
-from .feature_extractor import FeatureExtractor
-
-from .dataset import DataModule
-from . import datamodule
--- csr/module/dataset/__init__.py
+++
@@ -1,9 +0,0 @@
-# from ..utils.convert_activation import *
-# from ..utils.metrics import *
-from .celebA_dataset import CelebA, CelebAGender
-from .waterbirds_dataset import Waterbirds
-from .colored_mnist_dataset import ColoredMNIST
-from .concept_dataset import ConceptDataset
-from .spuco_dataset import Dogs
-from .catdog_dataset import CatDog
-from .data_module import DataModule

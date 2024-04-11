1ec3b9156ade2524dcdeed9103499bb0d1a9759c
Sunghwan Joo <shjoo840@gmail.com>
2024-04-11 19:03:24 +0900

    Joshua840/issue5/minor (#10)
    * my work is disappeared TT
    
    * temporal commit
    
    * celeba-collar ERM is trainable
    
    * gdro updated / celeba_collar_concept completed
    
    * shell script file is updated.
    
    * celeba_collar_concepts_v2 added
    
    * minor) script file and ipynb result file has been updated
    
    * Waterbirds added
    
    * tmp
    
    * experiments updated
    
    * pt file added
    
    * merged
    
    * update model_g
    
    * updated model_g
    
    ---------
    
    Co-authored-by: SunghwanJoo <rmrisforbidden@gmail.com>

--- scripts/CelebaCollar/1_feature_extraction.sh
+++ scripts/CelebaCollar/1_feature_extraction.sh
@@ -8,6 +8,6 @@ CUDA_VISIBLE_DEVICES=0 python3 -m csr.main_run fit \
  --model.save_root /media/disk2/Data/Features \
  --model.nimg_per_concept 128 \
  --trainer.max_epochs 100 \
- --trainer.check_val_every_n_epoch 1 \
+ --trainer.check_val_every_n_epoch 100 \
  --trainer.num_sanity_val_steps -1 \
  --model.num_workers 8 \
--- scripts/CelebaCollar/6_2_calculate_standard_cavs.sh
+++ scripts/CelebaCollar/6_2_calculate_standard_cavs.sh
@@ -8,7 +8,7 @@
 for CAV_TYPE in svm signal
 do
 CUDA_VISIBLE_DEVICES=0 python -m csr.main_cav \
---dataset celeba_collar_concepts \
+--dataset celeba_collar_concepts_v2 \
 --model_name convnext_t \
 --cav_type $CAV_TYPE \
 --root /media/disk2/Data \
